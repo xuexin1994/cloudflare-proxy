acd99fc277fe7cd27cb6ae81fd7f55e4770d6b72
ace <516507190@qq.com>
2023-07-25 02:28:28 +0800

    配置openai转发

--- _workers.js
+++ _workers.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
         const url = new URL(request.url);
-        url.host = "https://chat-gpt-next-web-vercel-ekluh2wlb-xuexin1994.vercel.app";
+        url.host = "api.openai.com";
         return fetch(url, {
             headers: request.headers,
             method: request.method,
